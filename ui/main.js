9ef57e1ce023db7e21ac71647164c89d690fd822
AndeDivya <divyatulasi93@gmail.com>
2017-09-11 19:36:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -51,7 +51,8 @@ submitButton.onclick =function()
         {
             if(request.status == 200)
             {
-                 var names = ['name1', 'name2', 'name3'];
+                 var names = request.responseText;
+                 names = JSON.parse(names);
                  var list = '';
                  for (var i=0; i<names.length; i++)
                      {
