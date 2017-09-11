41b2829c695104acbe7ed7e7ed2632cce462a8f4
AndeDivya <divyatulasi93@gmail.com>
2017-09-11 15:24:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -136,7 +136,7 @@ app.get('/ui/style.css', function (req, res) {
 
 app.get('/:articleName', function (req, res){
     var articleName = req.params.articleName;
-   res.send(createTemplate(articles[articleName]));
+   res.send(createTemplate(elements[articleName]));
 });
 
 
