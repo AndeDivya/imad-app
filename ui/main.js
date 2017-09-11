b0bf4fccad668cc7ba185065797bce7f9cace93c
AndeDivya <divyatulasi93@gmail.com>
2017-09-11 17:59:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,11 +15,24 @@ img.onclick = function()
 
 var button = document.getElementById("counter");
 
-var counter =0;
+
 button.onclick = function()
 {
+    var request = new XMLHttprequest();
     
-     counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
+    request.onreadystatechange = function() 
+    {
+        if(request.readystate == XMLHttpRequest.DONE)
+        {
+            if(request.status == 200)
+            {
+                var counter = request.responseText;
+                 var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+    };
+    
+   request.open('GET', 'http://divyatulasi93.imad.hasura-app.io/counter', true );
+   request.send(null);
 };
