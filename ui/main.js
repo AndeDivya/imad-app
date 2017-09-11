e7589a91c050571f5265af3eb68107467d38e0b8
AndeDivya <divyatulasi93@gmail.com>
2017-09-11 15:53:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,9 @@
 console.log('Loaded!');
+var img = document.getElementById("madi");
+img.onclick = function()
+{
+    img.style.marginLeft = '100px';
+    
+};
+var element = document.getElementById("main-text");
+element.innetHtml = "New Content";
