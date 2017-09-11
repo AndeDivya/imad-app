a75098a8f0f0e3b1daf8703ec27b838d7738b616
AndeDivya <divyatulasi93@gmail.com>
2017-09-11 11:57:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/one', function (req, res){
+    res.send("Hi this is one request");
+});
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
