a655d564fd95ab22ede23ef3da3d48ee875476b9
AndeDivya <divyatulasi93@gmail.com>
2017-09-11 15:59:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,14 @@
 console.log('Loaded!');
 var img = document.getElementById("madi");
+function moveRight()
+{
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
+}
+var marginleft = 0;
 img.onclick = function()
 {
-    img.style.marginLeft = '100px';
+    var interval = setInterval(moveRight, 100);
     
 };
 var element = document.getElementById("main-text");
